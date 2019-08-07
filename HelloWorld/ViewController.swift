e4c8019f38c90a9c7ab99bb5e757ba716152f347
Yshintaku <chunofam@gmail.com>
2019-08-07 11:31:44 +0900

    first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 新宅祐美 on 2019/08/06.
+//  Copyright © 2019 Yumi.Shintaku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
